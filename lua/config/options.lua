d4593b1dc5b6cc3b6c3eab15f1be67a8e955308c
kat01 <kat01@katt.com>
2026-04-21 12:40:40 +0600

    new dawn

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- Limit the width of the text to prevent horizontal scrolling in Niri
--- lua/config/options.lua
-vim.opt.laststatus = 3 -- Global statusline (looks cleaner in tiled layouts)
-vim.opt.pumheight = 10 -- Max height of pop-up menu (keeps it from hitting Niri edges)
-vim.opt.winbar = "%f" -- Shows file path at the top of the window instead of tabs
